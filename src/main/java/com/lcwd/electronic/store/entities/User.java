09df67bc30ad1c889608c9e17f9dd072419b8cd3
sparta-prog <debarun285@gmail.com>
2024-11-15 10:00:47 +0530

    Added User Entity

--- src/main/java/com/lcwd/electronic/store/entities/User.java
+++ src/main/java/com/lcwd/electronic/store/entities/User.java
@@ -1,8 +1,35 @@
 package com.lcwd.electronic.store.entities;
 
-import jakarta.persistence.Entity;
+import lombok.*;
 
+import javax.persistence.*;
+import java.util.*;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
+@Table(name = "users")
 public class User {
-    
+    @Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String userId;
+
+    @Column(name = "user_name")
+    private String name;
+
+    @Column(name = "user_email", unique = true)
+    private String email;
+
+    @Column(name = "user_password", length = 500)
+    private String password;
+
+    private String gender;
+
+    @Column(length = 1000)
+    private String about;
+
+    @Column(name = "user_image_name")
+    private String imageName;
 }
